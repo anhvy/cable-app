daf17d04f6f96042e1ce04384e7a3bae2d787f46
= <nguyenvuonganhvy@gmail.com>
2018-03-03 02:56:03 +0700

    Added action cable configuraion for production

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,5 @@ module CableApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.action_cable.url = "ws://localhost:3334/cable"
   end
 end
